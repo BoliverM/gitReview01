9f41ffa50c37e74a07b1c754553f75b49eb48a0f
BoliverM <boliponget@gmail.com>
2022-06-07 09:49:44 -0400

    2nd commit

--- src/gitReview/gitReview01.java
+++ src/gitReview/gitReview01.java
@@ -1,5 +1,13 @@
 package gitReview;
 
 public class gitReview01 {
+	public static void main(String[] args) {
+		// TODO Auto-generated constructor stub
+		
+		System.out.println("today we learn github");
+		System.out.println("today is a very good day");
+		
+		System.out.println("today is a very good day");
+	}
 
 }
